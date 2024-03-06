a068e931b901a0922ed9b26022cb10a31a4ef332
M4tt1-Coder <geimat75@gmail.com>
2024-03-06 22:01:52 +0100

    Route restructuring + options pages
    I added a new route logic for the settings of the game and their implementation.

---
+++ Enums/PlayTimeMode.cs
@@ -0,0 +1,19 @@
+namespace Chess_API.Enums;
+
+/// <summary>
+/// 
+/// </summary>
+public static class PlayTimeMode
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static readonly string ThreeMinutes = "03:00";
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public static readonly string TenMinutes = "10:00";
+    
+    public static readonly string 
+}
